498dadfb661a4745cf70e303c8262a64aaa95f54
mp1325 <malakhip11@gmail.com>
2023-10-03 09:05:13 -0700

    To Do list is functionable

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+console.log('Hello');
+
+let button = document.querySelector('#submit');
+let input = document.querySelector('#input');
+let list = document.getElementById('list');
+
+
+function addItem(){
+    let input2 = input.value;
+
+    if(input2 !== null && input2 !== ''){
+        let newItem = document.createElement('li');
+        newItem.textContent = input2;
+        console.log("newItem", newItem);
+        console.log('list', list);
+        list.appendChild(newItem);
+        input.value = "";
+        
+    }
+}
+
+button.addEventListener('click', addItem)
 
